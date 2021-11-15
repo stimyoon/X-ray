dd8ac821110d072df926a2f0e52c902f00b3cd6e
Tim Yoon <stimyoon@gmail.com>
2021-11-14 21:39:14 -0500

    Initial Commit

---
+++ Shared/X_rayApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_rayApp.swift
+//  Shared
+//
+//  Created by Tim Yoon on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct X_rayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
