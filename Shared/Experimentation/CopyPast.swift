275d882f24204f98427dafd529225fc09b023bbf
Tim Yoon <stimyoon@gmail.com>
2021-12-15 06:44:08 -0500

    Cleaned up file locations.

---
+++ Shared/Experimentation/CopyPast.swift
@@ -0,0 +1,20 @@
+//
+//  CopyPast.swift
+//  X-ray
+//
+//  Created by Tim Yoon on 12/15/21.
+//
+
+import SwiftUI
+
+struct CopyPast: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CopyPast_Previews: PreviewProvider {
+    static var previews: some View {
+        CopyPast()
+    }
+}
